de7aa6cee55c97633a34aed5479f6ee68681c355
Wiktor Mociun <voter101@gmail.com>
2014-12-14 19:09:41 +0100

    Controller test

--- utests/test/controllers/weird_enterprise_stuffs_controller_test.rb
+++ utests/test/controllers/weird_enterprise_stuffs_controller_test.rb
@@ -2,7 +2,8 @@ require 'test_helper'
 
 class WeirdEnterpriseStuffsControllerTest < ActionController::TestCase
   setup do
-    @weird_enterprise_stuff = weird_enterprise_stuffs(:one)
+    @good_item = weird_enterprise_stuffs(:good)
+    @bad_item  = weird_enterprise_stuffs(:bad)
   end
 
   test "should get index" do
@@ -17,31 +18,37 @@ class WeirdEnterpriseStuffsControllerTest < ActionController::TestCase
   end
 
   test "should create weird_enterprise_stuff" do
-    assert_difference('WeirdEnterpriseStuff.count') do
-      post :create, weird_enterprise_stuff: { priority: @weird_enterprise_stuff.priority }
+    assert_difference('WeirdEnterpriseStuff.count', 1) do
+      post :create, weird_enterprise_stuff: { priority: @good_item.priority }
+      assert_redirected_to weird_enterprise_stuff_path(assigns(:weird_enterprise_stuff))
     end
+  end
 
-    assert_redirected_to weird_enterprise_stuff_path(assigns(:weird_enterprise_stuff))
+  test "should get an error while creating bad item" do
+    assert_no_difference('WeirdEnterpriseStuff.count') do
+      post :create, weird_enterprise_stuff: { priority: @bad_item.priority }
+    end
   end
 
   test "should show weird_enterprise_stuff" do
-    get :show, id: @weird_enterprise_stuff
+    get :show, id: @good_item
     assert_response :success
   end
 
   test "should get edit" do
-    get :edit, id: @weird_enterprise_stuff
+    get :edit, id: @good_item
     assert_response :success
   end
 
+
   test "should update weird_enterprise_stuff" do
-    patch :update, id: @weird_enterprise_stuff, weird_enterprise_stuff: { priority: @weird_enterprise_stuff.priority }
+    patch :update, id: @good_item, weird_enterprise_stuff: { priority: @good_item.priority }
     assert_redirected_to weird_enterprise_stuff_path(assigns(:weird_enterprise_stuff))
   end
 
   test "should destroy weird_enterprise_stuff" do
     assert_difference('WeirdEnterpriseStuff.count', -1) do
-      delete :destroy, id: @weird_enterprise_stuff
+      delete :destroy, id: @good_item
     end
 
     assert_redirected_to weird_enterprise_stuffs_path
