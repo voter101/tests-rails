0faad5d3a3f76f72d856451a9bddf4ff33582081
Wiktor Mociun <voter101@gmail.com>
2014-12-14 18:51:46 +0100

    Test model validation

--- utests/app/models/weird_enterprise_stuff.rb
+++ utests/app/models/weird_enterprise_stuff.rb
@@ -1,2 +1,8 @@
 class WeirdEnterpriseStuff < ActiveRecord::Base
+  validate :asap_is_only_priority
+
+  private
+  def asap_is_only_priority
+    errors.add(:priority, "ASAP is the only way") unless priority == "ASAP"
+  end
 end
--- utests/test/models/weird_enterprise_stuff_test.rb
+++ utests/test/models/weird_enterprise_stuff_test.rb
@@ -1,7 +1,22 @@
 require 'test_helper'
 
 class WeirdEnterpriseStuffTest < ActiveSupport::TestCase
-  test "the truth" do
-    assert true
+  test "Priority field validation" do
+    priority_validation_with_bad_data
+    priority_validation_with_good_data
+  end
+
+  private
+  def priority_validation_with_bad_data
+    exception = assert_raise ActiveRecord::RecordInvalid do
+      WeirdEnterpriseStuff.create!
+    end
+    assert_equal exception.message, "Validation failed: Priority ASAP is the only way"
+  end
+
+  def priority_validation_with_good_data
+    assert_nothing_raised do
+      WeirdEnterpriseStuff.create(priority: "ASAP")
+    end
   end
 end
