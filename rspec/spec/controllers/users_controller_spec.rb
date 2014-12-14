b58a44763778c4d081622ba3870dceb963402d0f
Wiktor Mociun <voter101@gmail.com>
2014-12-14 22:17:42 +0100

    Some fun with status codes

--- rspec/spec/controllers/users_controller_spec.rb
+++ rspec/spec/controllers/users_controller_spec.rb
@@ -7,12 +7,14 @@ describe UsersController do
     specify "successful" do
       expect {
         post :create, { user: valid_attributes }
+        response.status.should eq(302)
       }.to change(User, :count).by(1)
     end
 
     specify "unsuccessful" do
       expect {
         post :create, { user: invalid_attributes }
+        response.status.should eq(200)
       }.to_not change(User, :count)
     end
   end
